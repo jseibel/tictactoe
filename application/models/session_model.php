a3b9e2e66fe4575e24638454ee8c33b68bafadaa
John Seibel <jseibel3@gmail.com>
2011-04-22 12:03:32 -0400

    added custom Session.php to app/lib...removed ip_address and user_agent logging

--- application/models/session_model.php
+++ application/models/session_model.php
@@ -3,13 +3,13 @@ class Session_model extends CI_Model {
 
   public function __construct(){
     parent::__construct();
-/*    $this->db->query("CREATE TABLE IF NOT EXISTS 'session_rec' (
+    $this->db->query("CREATE TABLE IF NOT EXISTS session_rec(
                       session_id varchar(40) DEFAULT '0' NOT NULL,
                       PRIMARY KEY(session_id),
                       survey_info varchar(40),
                       last_activity int(10) unsigned DEFAULT '0' NOT NULL
                     )");
- */
+ 
   }
 
 }
