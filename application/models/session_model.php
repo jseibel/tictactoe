b1beac8d6d335da08294ade9171447a008002543
John Seibel <jseibel3@gmail.com>
2011-04-22 11:25:21 -0400

    added a session model to track user interaction

---
+++ application/models/session_model.php
@@ -0,0 +1,18 @@
+<?php
+class Session_model extends CI_Model {
+
+  public function __construct(){
+    parent::__construct();
+/*    $this->db->query("CREATE TABLE IF NOT EXISTS 'session_rec' (
+                      session_id varchar(40) DEFAULT '0' NOT NULL,
+                      PRIMARY KEY(session_id),
+                      survey_info varchar(40),
+                      last_activity int(10) unsigned DEFAULT '0' NOT NULL
+                    )");
+ */
+  }
+
+}
+
+
+?>
